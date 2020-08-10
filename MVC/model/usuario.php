dad3b2b5b9a590fe0291d883c4d6cc0ded206981
Luan Aguiar <luanaguiar16@live.com>
2020-08-10 13:24:06 -0300

    Commit criação da classe de usuários.

--- MVC/model/usuario.php
+++ MVC/model/usuario.php
@@ -3,9 +3,42 @@
 class Usuario {
 
     private $id_usuario;
+    private $nome_usuario;
+    private $email;
+    private $senha;
 
-    public getId_Usuario(){
-        return $this->$id_usuario;
+    // Getters
+    public function getId_Usuario(){
+        return $this->id_usuario;
+    }
+
+    public function getNome_Usuario(){
+        return $this->nome_usuario;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+
+    public function getSenha(){
+        return $this->senha;
+    }
+
+    //Setters
+    public function setId_Usuario($id){
+        $this->id_usuario = $id;
+    }
+
+    public function setNome_Usuario($nome){
+        $this->nome_usuario = $nome;
+    }
+
+    public function setEmail($email){
+        $this->email = $email;
+    }
+
+    public function setSenha($senha){
+        $this->senha = $senha;
     }
 
 }
